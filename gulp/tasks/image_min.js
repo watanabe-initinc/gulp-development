fb69242b1df930e04c3f514258e2c5bf2e13a1b2
watanabe-initinc <watanabe@initinc.co.jp>
2020-05-21 11:29:22 +0900

    タスク実行時、ファイル圧縮履歴を出力するようオプション付与

--- gulp/tasks/image_min.js
+++ gulp/tasks/image_min.js
@@ -1,38 +1,45 @@
-import plugin from '../plugin';
-import {
-  image_min as imgminConfig
-} from '../config';
+import plugin from "../plugin";
+import { image_min as imgminConfig } from "../config";
 
 const $ = plugin;
 const env = process.env.NODE_ENV;
 let outputDir = imgminConfig.build;
 
-if (env === 'development') {
+if (env === "development") {
   outputDir = imgminConfig.dest;
 }
 
 export function image_min() {
-  return $.gulp.src(imgminConfig.input)
-    .pipe($.image_min([
-      $.image_min.gifsicle({
-        interlaced: true
-      }),
-      $.image_min.mozjpeg({
-        quality: 75,
-        progressive: true
-      }),
-      $.image_min.optipng({
-        optimizationLevel: 7
-      }),
-      $.image_min.svgo({
-        plugins: [{
-            removeViewBox: true
-          },
-          {
-            cleanupIDs: false
-          }
-        ]
-      })
-    ]))
+  return $.gulp
+    .src(imgminConfig.input)
+    .pipe(
+      $.image_min(
+        [
+          $.image_min.gifsicle({
+            interlaced: true,
+          }),
+          $.image_min.mozjpeg({
+            quality: 75,
+            progressive: true,
+          }),
+          $.image_min.optipng({
+            optimizationLevel: 7,
+          }),
+          $.image_min.svgo({
+            plugins: [
+              {
+                removeViewBox: true,
+              },
+              {
+                cleanupIDs: false,
+              },
+            ],
+          }),
+        ],
+        {
+          verbose: true,
+        }
+      )
+    )
     .pipe($.gulp.dest(outputDir));
 }
